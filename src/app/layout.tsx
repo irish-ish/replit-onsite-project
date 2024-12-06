42bcefb981b66af2df46430560ed9087aa3f7fc1
Brian Kennedy <kennedy.brianpatrick@gmail.com>
2024-12-05 20:54:17 -0800

    Initial commit: Setup Next.js project with Yarn, TypeScript, Tailwind, ESLint, and Shadcn-UI

---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import { ThemeProvider } from "@/providers/theme-provider";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { ReactQueryProvider } from "@/providers/react-query-provider";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Replit On-site",
+  description: "Replit On-site Interview",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ReactQueryProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </ReactQueryProvider>
+      </body>
+    </html>
+  );
+}
